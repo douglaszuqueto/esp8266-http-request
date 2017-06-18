585d9cf83f8711565f1c099e88900dd7554742cf
douglaszuqueto <douglas.zuqueto@gmail.com>
2017-06-18 17:22:00 -0300

    add delete.ino

---
+++ esp8266/delete.ino
@@ -0,0 +1,99 @@
+// ############# LIBRARIES ############### //
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+// ############# VARIABLES ############### //
+
+const char* SSID = ""; // rede wifi
+const char* PASSWORD = ""; // senha da rede wifi
+
+String BASE_URL = "http://192.168.15.3:3000/";
+
+// ############# PROTOTYPES ############### //
+
+void initSerial();
+void initWiFi();
+void httpRequest(String path);
+
+// ############### OBJECTS ################# //
+
+WiFiClient client;
+HTTPClient http;
+
+// ############## SETUP ################# //
+
+void setup() {
+  initSerial();
+  initWiFi();
+}
+
+// ############### LOOP ################# //
+
+void loop() {
+  Serial.println("[DELETE] /sensors - sending request...");
+  Serial.println("");
+
+  httpRequest("sensors/1234");
+
+  Serial.println("");
+  delay(1000);
+
+}
+
+// ############# HTTP REQUEST ################ //
+
+void httpRequest(String path)
+{
+  String payload = makeRequest(path);
+
+  if (!payload) {
+    return;
+  }
+
+  Serial.println("##[RESULT]## ==> " + payload);
+
+}
+
+String makeRequest(String path)
+{
+  http.begin(BASE_URL + path);
+  int httpCode = http.sendRequest("DELETE");
+
+  if (httpCode < 0) {
+    Serial.println("request error - " + httpCode);
+    return "";
+
+  }
+
+  if (httpCode != HTTP_CODE_OK) {
+    return "";
+  }
+
+  String response =  http.getString();
+  http.end();
+
+  return response;
+}
+
+// ###################################### //
+
+// implementacao dos prototypes
+
+void initSerial() {
+  Serial.begin(115200);
+}
+
+void initWiFi() {
+  delay(10);
+  Serial.println("Conectando-se em: " + String(SSID));
+
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Conectado na Rede " + String(SSID) + " | IP => ");
+  Serial.println(WiFi.localIP());
+}
